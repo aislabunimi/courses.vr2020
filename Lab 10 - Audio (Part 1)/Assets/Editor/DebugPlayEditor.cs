7cdd0f506231a3a39527a1b28088c18cadc8fbf4
Manuel <manuel.pezzera@unimi.it>
2021-05-19 16:20:14 +0200

    Added lab 10

---
+++ Lab 10 - Audio (Part 1)/Assets/Editor/DebugPlayEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DebugPlay))]
+public class DebugPlayEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DebugPlay myTarget = (DebugPlay) target;
+        
+        if (GUILayout.Button("Play sound")) myTarget.Play();
+        if (GUILayout.Button("Play sound with reverb")) myTarget.PlayWithReverb();
+        if (GUILayout.Button("Play sound without reverb")) myTarget.PlayWithoutReverb();
+    }
+}
---
+++ Lab 10 - Audio (Part 1)/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Class that will spawn the targets
+/// </summary>
+public class TargetManager : MonoBehaviour
+{
+    private int MIN_TIME = 1, MAX_TIME = 4;
+    private int MIN_LIFE = 5, MAX_LIFE = 12;
+
+    private int MIN_X = -10, MAX_X = 10;
+    private int MIN_Y = -10, MAX_Y = 10;
+    private int MIN_Z = 12, MAX_Z = 20;
+
+    private int MIN_SCALE = 5, MAX_SCALE = 10;
+
+    [SerializeField] private GameObject[] targetsPrefabs;
+    
+    private void Start()
+    {
+        // To complete
+    }
+}
